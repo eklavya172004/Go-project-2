2fbce69e342232479c26bf76a9f0a18321c69286
Eklavya Nath <optimusprime172004@gmail.com>
2025-05-29 05:39:20 +0530

    gogo

--- bookstore/go.mod
+++ bookstore/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
--- bookstore/pkg/config/app.go
+++ bookstore/pkg/config/app.go
@@ -4,12 +4,26 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 		"fmt"
+		  "github.com/joho/godotenv"
+		  	"os"
 )
 
 var db *gorm.DB
 
 func Connect() {
-	dsn := "go_user:YourPassword123!@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		user, pass, host, port, name)
 
 	d, err := gorm.Open("mysql", dsn)
 	if err != nil {
